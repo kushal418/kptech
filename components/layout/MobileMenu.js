0b86ded7633a5b338d7893dd08992173598a3b93
kushal418 <kushalpawar418@gmail.com>
2025-08-05 21:08:08 +0530

    bug solve 1

--- components/layout/MobileMenu.js
+++ components/layout/MobileMenu.js
@@ -1,166 +1,37 @@
 import Link from "next/link";
-import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
-export default function MobileMenu() {
-  const [isActive, setIsActive] = useState({
-    status: false,
-    key: "",
-    subkey: "",
-  });
+export default function MobileMenu({ isMobileMenu, setShowMenu }) {
+    const router = useRouter();
 
-  const router = useRouter();
-
-  // 👇 Reset menu on route change
-  useEffect(() => {
-    const handleRouteChange = () => {
-      setIsActive({
-        status: false,
-        key: "",
-        subkey: "",
-      });
+    const handleClose = () => {
+        setShowMenu(false);
     };
 
-    router.events.on("routeChangeComplete", handleRouteChange);
+    return (
+        <div className={`mobile-menu ${isMobileMenu ? "show" : ""}`}>
+            <div className="menu-backdrop" onClick={handleClose}></div>
+            <div className="menu-box">
+                <div className="close-btn" onClick={handleClose}><i className="flaticon-close"></i></div>
 
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
-  const handleToggle = (key) => {
-    setIsActive((prev) => ({
-      ...prev,
-      key: prev.key === key ? "" : key,
-      subkey: "",
-    }));
-  };
-
-  const handleSubToggle = (subkey) => {
-    setIsActive((prev) => ({
-      ...prev,
-      subkey: prev.subkey === subkey ? "" : subkey,
-    }));
-  };
-
-  return (
-    <>
-      <ul className="navigation">
-        {/* Home */}
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-
-        {/* Courses */}
-        <li className="menu-item-has-children">
-          <Link href="#">Courses</Link>
-          <ul
-            className="sub-menu"
-            style={{ display: isActive.key === 2 ? "block" : "none" }}
-          >
-            <li className="menu-item-has-children">
-              <Link href="/courses">Frontend Development</Link>
-              <ul
-                className="sub-menu"
-                style={{
-                  display: isActive.subkey === "frontend" ? "block" : "none",
-                }}
-              >
-                <li><Link href="/course/1">HTML5 / CSS3</Link></li>
-                <li><Link href="/course/2">JavaScript</Link></li>
-                <li><Link href="/course/4">React.js</Link></li>
-                <li><Link href="/course/6">Angular</Link></li>
-                <li><Link href="/course/7">Vue.js</Link></li>
-              </ul>
-              <div
-                className={isActive.subkey === "frontend" ? "dropdown-btn open" : "dropdown-btn"}
-                onClick={() => handleSubToggle("frontend")}
-              >
-                <span className="plus-line" />
-              </div>
-            </li>
-
-            {/* Backend */}
-            <li className="menu-item-has-children">
-              <Link href="/courses">Backend Development</Link>
-              <ul
-                className="sub-menu"
-                style={{
-                  display: isActive.subkey === "backend" ? "block" : "none",
-                }}
-              >
-                <li><Link href="/course/11">Node.js</Link></li>
-                <li><Link href="/course/16">PHP</Link></li>
-                <li><Link href="/course/13">ASP.NET Core</Link></li>
-                <li><Link href="/course/14">Java</Link></li>
-                <li><Link href="/course/15">Python (Django/Flask)</Link></li>
-              </ul>
-              <div
-                className={isActive.subkey === "backend" ? "dropdown-btn open" : "dropdown-btn"}
-                onClick={() => handleSubToggle("backend")}
-              >
-                <span className="plus-line" />
-              </div>
-            </li>
-
-            {/* Database */}
-            <li className="menu-item-has-children">
-              <Link href="/courses">Database</Link>
-              <ul
-                className="sub-menu"
-                style={{
-                  display: isActive.subkey === "database" ? "block" : "none",
-                }}
-              >
-                <li><Link href="/course/17">MySQL</Link></li>
-                <li><Link href="/course/18">MongoDB</Link></li>
-              </ul>
-              <div
-                className={isActive.subkey === "database" ? "dropdown-btn open" : "dropdown-btn"}
-                onClick={() => handleSubToggle("database")}
-              >
-                <span className="plus-line" />
-              </div>
-            </li>
-
-            {/* Designing */}
-            <li className="menu-item-has-children">
-              <Link href="/courses">Designing</Link>
-              <ul
-                className="sub-menu"
-                style={{
-                  display: isActive.subkey === "designing" ? "block" : "none",
-                }}
-              >
-                <li><Link href="/course/19">Figma</Link></li>
-                <li><Link href="/course/20">Adobe XD</Link></li>
-              </ul>
-              <div
-                className={isActive.subkey === "designing" ? "dropdown-btn open" : "dropdown-btn"}
-                onClick={() => handleSubToggle("designing")}
-              >
-                <span className="plus-line" />
-              </div>
-            </li>
-          </ul>
-          <div
-            className={isActive.key === 2 ? "dropdown-btn open" : "dropdown-btn"}
-            onClick={() => handleToggle(2)}
-          >
-            <span className="plus-line" />
-          </div>
-        </li>
-
-        {/* About */}
-        <li>
-          <Link href="/about-us">About us</Link>
-        </li>
-
-        {/* Contact */}
-        <li>
-          <Link href="/contact">Contact us</Link>
-        </li>
-      </ul>
-    </>
-  );
+                <nav className="menu-outer">
+                    <ul className="navigation">
+                        <li>
+                            <Link href="/" onClick={handleClose}>Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/about" onClick={handleClose}>About</Link>
+                        </li>
+                        <li>
+                            <Link href="/courses" onClick={handleClose}>Courses</Link>
+                        </li>
+                        <li>
+                            <Link href="/contact" onClick={handleClose}>Contact</Link>
+                        </li>
+                        {/* Add more links here if needed */}
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
 }
--- components/layout/header/Header1.js
+++ components/layout/header/Header1.js
@@ -1,10 +1,24 @@
-import Link from "next/link"
-import MobileMenu from "../MobileMenu"
+import Link from "next/link";
+import MobileMenu from "../MobileMenu";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
-export default function Header1({ scroll, isMobileMenu, handleMobileMenu }) {
+export default function Header1({ scroll, isMobileMenu, handleMobileMenu, setShowMenu }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChange = () => {
+            setShowMenu(false);
+        };
+
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router, setShowMenu]);
+
     return (
         <>
-        
             <header>
                 <div id="sticky-header" className={`tg-header__area transparent-header ${scroll ? "sticky-menu" : ""}`}>
                     <div className="container custom-container">
@@ -16,121 +30,73 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu }) {
                                         <div className="logo">
                                             <Link href="/"><img src="/assets/img/logo/logo.png" alt="Logo" /></Link>
                                         </div>
-                                     <div className="tgmenu__navbar-wrap tgmenu__main-menu d-none d-xl-flex">
-                                        <ul className="navigation" style={{ margin: '0px 0px 0px 340px' }}>
-                                          <li className="active">
-                                            <Link href="#">Home</Link>
-                                          </li>
-
-                                          {/* Courses */}
-                                          <li className="menu-item-has-children">
-                                        <Link href="#">Courses</Link>
-                                        <ul className="sub-menu" style={{ minWidth: "290px", position: "absolute", zIndex: 999 }}>
-
-                                          {/* Frontend Development */}
-                                          <li className="">
-                                            <Link href="/courses">
-                                              Frontend Development
-                                              <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
-                                            </Link>
-                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
-                                              <li><Link href="/course/1">HTML5 / CSS3</Link></li>
-                                              <li><Link href="/course/2 ">JavaScript</Link></li>
-                                              <li><Link href="/course/4">React.js</Link></li>
-                                              <li><Link href="/course/6">Angular</Link></li>
-                                              <li><Link href="/course/7">Vue.js</Link></li>
-                                            </ul>
-                                          </li>
-
-                                          {/* Backend Development */}
-                                          <li className="">
-                                            <Link href="/courses">
-                                              Backend Development
-                                              <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
-                                            </Link>
-                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
-                                              <li><Link href="/course/11">Node.js</Link></li>
-                                              <li><Link href="/course/16">PHP</Link></li>
-                                              <li><Link href="/course/13">ASP.NET Core</Link></li>
-                                              <li><Link href="/course/14">Java</Link></li>
-                                              <li><Link href="/course/15">Python (Django/Flask)</Link></li>
-                                            </ul>
-                                          </li>
-
-                                          {/* Database */}
-                                          <li className="">
-                                            <Link href="/courses">
-                                              Database
-                                              <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
-                                            </Link>
-                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
-                                              <li><Link href="/course/17">MySQL</Link></li>
-                                              <li><Link href="/course/18">MongoDB</Link></li>
-                                            </ul>
-                                          </li>
-                                          {/* Designing */}
-                                          <li className="">
-                                            <Link href="/courses">
-                                              Designing
-                                              <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
-                                            </Link>
-                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
-                                              <li><Link href="/course/19">Figma</Link></li>
-                                              <li><Link href="/course/20">Adobe XD</Link></li>
-                                            </ul>
-                                          </li>
-                                        </ul>
-                                      </li>
-
-
-                                          {/* Pages */}
-                                          {/* <li className="menu-item-has-children">
-                                            <Link href="#">Pages</Link>
-                                            <ul className="sub-menu">
-                                              <li><Link href="/about-us">About Us</Link></li>
-                                              <li><Link href="/instructors">Our Instructors</Link></li>
-                                              <li><Link href="/instructor-details">Instructor Details</Link></li>
-                                              <li><Link href="/mentors">Our Mentors</Link></li>
-                                              <li><Link href="/events">Our Events</Link></li>
-                                              <li><Link href="/events-details">Event Details</Link></li>
-                                              <li><Link href="/faq">Asked Question</Link></li>
-                                              <li><Link href="/404">404 Page</Link></li>
-                                              <li><Link href="/contact">Contact</Link></li>
-                                            </ul>
-                                          </li> */}
-
-                                          {/* Shop */}
-                                          <li className="">
-                                            <Link href="about-us">About us</Link>
-                                          
-                                          </li>
-                                          <li className="">
-                                            <Link href="contact">Contact us</Link>
-                                          
-                                          </li>
-
-                                          {/* Blog */}
-                                          {/* <li className="menu-item-has-children">
-                                            <Link href="#">Blog</Link>
-                                            <ul className="sub-menu">
-                                              <li><Link href="/blog-grid">Blog Grid</Link></li>
-                                              <li><Link href="/blog">Blog Standard</Link></li>
-                                              <li><Link href="/blog-details">Blog Details</Link></li>
-                                            </ul>
-                                          </li> */}
-                                        </ul>
-                                      </div>
-
-                                         <div className="tgmenu__action">
-                                            <ul className="list-wrap">
-                                                <li className="header-phone d-none d-lg-block d-xl-none d-xxl-block"><Link href="/tel:+919824996227"><i className="flaticon-phone-call" />
-                                                    98-24-996227</Link></li>
-                                                <li className="header-btn login-btn"><Link href="/contact" className="btn">Log in </Link>
+                                        <div className="tgmenu__navbar-wrap tgmenu__main-menu d-none d-xl-flex">
+                                            <ul className="navigation" style={{ margin: '0px 0px 0px 340px' }}>
+                                                <li className="active"><Link href="#">Home</Link></li>
+                                                <li className="menu-item-has-children">
+                                                    <Link href="#">Courses</Link>
+                                                    <ul className="sub-menu" style={{ minWidth: "290px", position: "absolute", zIndex: 999 }}>
+                                                        <li>
+                                                            <Link href="/courses">
+                                                                Frontend Development
+                                                                <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
+                                                            </Link>
+                                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
+                                                                <li><Link href="/course/1">HTML5 / CSS3</Link></li>
+                                                                <li><Link href="/course/2">JavaScript</Link></li>
+                                                                <li><Link href="/course/4">React.js</Link></li>
+                                                                <li><Link href="/course/6">Angular</Link></li>
+                                                                <li><Link href="/course/7">Vue.js</Link></li>
+                                                            </ul>
+                                                        </li>
+                                                        <li>
+                                                            <Link href="/courses">
+                                                                Backend Development
+                                                                <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
+                                                            </Link>
+                                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
+                                                                <li><Link href="/course/11">Node.js</Link></li>
+                                                                <li><Link href="/course/16">PHP</Link></li>
+                                                                <li><Link href="/course/13">ASP.NET Core</Link></li>
+                                                                <li><Link href="/course/14">Java</Link></li>
+                                                                <li><Link href="/course/15">Python (Django/Flask)</Link></li>
+                                                            </ul>
+                                                        </li>
+                                                        <li>
+                                                            <Link href="/courses">
+                                                                Database
+                                                                <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
+                                                            </Link>
+                                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
+                                                                <li><Link href="/course/17">MySQL</Link></li>
+                                                                <li><Link href="/course/18">MongoDB</Link></li>
+                                                            </ul>
+                                                        </li>
+                                                        <li>
+                                                            <Link href="/courses">
+                                                                Designing
+                                                                <span style={{ color: "gray", fontSize: "20px", float: "right", marginTop: "2px" }}>{'>'}</span>
+                                                            </Link>
+                                                            <ul className="sub-menu" style={{ minWidth: "240px", left: "100%", top: "0", position: "absolute" }}>
+                                                                <li><Link href="/course/19">Figma</Link></li>
+                                                                <li><Link href="/course/20">Adobe XD</Link></li>
+                                                            </ul>
+                                                        </li>
+                                                    </ul>
                                                 </li>
+                                                <li><Link href="about-us">About us</Link></li>
+                                                <li><Link href="contact">Contact us</Link></li>
+                                            </ul>
+                                        </div>
+                                        <div className="tgmenu__action">
+                                            <ul className="list-wrap">
+                                                <li className="header-phone d-none d-lg-block d-xl-none d-xxl-block">
+                                                    <Link href="/tel:+919824996227"><i className="flaticon-phone-call" /> 98-24-996227</Link>
+                                                </li>
+                                                <li className="header-btn login-btn"><Link href="/contact" className="btn">Log in </Link></li>
                                                 <li className="header-btn"><Link href="/bookdemo" className="btn">Book Demo</Link></li>
                                             </ul>
                                         </div>
-                                     
                                     </nav>
                                 </div>
                                 {/* Mobile Menu  */}
@@ -141,7 +107,7 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu }) {
                                             <Link href="/"><img src="/assets/img/logo/logo.png" alt="Logo" /></Link>
                                         </div>
                                         <div className="tgmobile__menu-outer">
-                                            <MobileMenu />
+                                            <MobileMenu setShowMenu={setShowMenu} />
                                         </div>
                                         <div className="tgmenu__action">
                                             <ul className="list-wrap">
@@ -168,5 +134,5 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu }) {
                 </div>
             </header>
         </>
-    )
+    );
 }
